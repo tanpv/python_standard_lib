798699119644ee8f57c2075cbdec3a3245537799
tanpv <phamtan500@gmail.com>
2016-08-02 15:26:25 +0700

    update text

--- text/text_wrap_fill.py
+++ text/text_wrap_fill.py
@@ -1,27 +1,18 @@
 import textwrap
 
-text = '''
-    The textwrap module can be used to format text for output in
-    situations where pretty-printing is desired. It offers
-    programmatic functionality similar to the paragraph wrapping
-    or filling features found in many text editors.
-    '''
-print 'original text :\n'
-print text
+text = '''The textwrap module can be used to format text for output in
+situations where pretty-printing is desired. It offers
+programmatic functionality similar to the paragraph wrapping
+or filling features found in many text editors.'''
 
+def print_wrap_text(text, width):
+	print textwrap.fill(text, width = width)
+	print '\n'
+	print textwrap.wrap(text, width = width)
+	print '\n'
 
-text_removed_indented = textwrap.dedent(text).strip()
-print 'text after rem:\n'
-print text_removed_indented
-print '\n\n'
+# width = 30
+print_wrap_text(text, 30)
 
-
-# use textwrap to format width = 30
-print 'format text width = 30 \n'
-print textwrap.fill(text_removed_indented, width = 30)
-print '\n\n'
-
-# use textwrap to format width = 70
-print 'format text width = 70 \n'
-print textwrap.fill(text_removed_indented, width = 70)
-print '\n\n'
+# width = 75
+print_wrap_text(text, 75)
